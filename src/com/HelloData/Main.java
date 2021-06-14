cd95fb179f6696e194f5e308b9a0e2a0f0b12d54
Olegionek <olegionek@gmail.com>
2021-06-14 17:05:03 +0300

    Initial commit

---
+++ src/com/HelloData/Main.java
@@ -0,0 +1,27 @@
+package com.HelloData;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+	ATypeName dt = new ATypeName();
+	DataOnly dat = new DataOnly();
+	dat.i = 4;
+	dat.d = 3.45;
+	dat.b = false;
+	System.out.println((dt.a) + " " + (dat.i) + " "+ (dat.d) + " " + (dat.b));
+	System.out.println(dat.i);
+	System.out.println(dat.d);
+	System.out.println(dat.b);
+
+    }
+}
+
+class ATypeName {
+    double a = 2.34;
+}
+class DataOnly {
+    int i;
+    double d;
+    boolean b;
+}
