c284851c1e064eef0b8dc9dcb0613cfe05ccb644
Olegionek <olegionek@gmail.com>
2021-06-14 17:05:03 +0300

    Initial commit

---
+++ src/com/HelloData/Main.java
@@ -0,0 +1,14 @@
+package com.HelloData;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+	ATypeName dt = new ATypeName();
+	System.out.println(dt.a);
+    }
+}
+
+class ATypeName {
+    double a = 2.34;
+}
