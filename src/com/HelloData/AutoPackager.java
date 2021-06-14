d353badf6366cea375677e6e89f7db8b1df760bd
Olegionek <olegionek@gmail.com>
2021-06-14 20:26:31 +0300

    Exercise N9 has been added

--- src/com/HelloData/AutoPackager.java
+++ src/com/HelloData/AutoPackager.java
@@ -1,7 +1,6 @@
 package com.HelloData;
-import java.util.*;
 
 //Exercise N9 Thinking on Java p.93
 public class AutoPackager {
-    Character ch = Character.valueOf('f');
+    Character ch = new Character('f');
 }
