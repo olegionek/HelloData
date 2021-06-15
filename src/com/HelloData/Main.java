8c6fe7ac2f190f246ccb561e4c8c010a021cbe18
Olegionek <olegionek@gmail.com>
2021-06-15 16:12:33 +0300

    New live

--- src/com/HelloData/Main.java
+++ src/com/HelloData/Main.java
@@ -3,7 +3,16 @@ import java.lang.*;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		float expFloat = 1.39e-43f;
+		expFloat = 1.39E-43f;
+		System.out.println(expFloat);
+		double expDouble = 47e47d;
+		double expDouble2 = 47e47;
+		long n2 = 200;
+		System.out.println(expDouble);
+		System.out.println(n2*expDouble);
 	}
 }
+
