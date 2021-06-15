8093b024098a05ff0c307616e8b9b5f117d19c37
Olegionek <olegionek@gmail.com>
2021-06-15 11:51:44 +0300

    New live

--- src/com/HelloData/Main.java
+++ src/com/HelloData/Main.java
@@ -1,19 +1,9 @@
 package com.HelloData;
-import java.util.*;
 import java.lang.*;
-import static com.HelloData.AutoPackager.*;
 
 public class Main {
 
     public static void main(String[] args) {
-    	Tank t1 = new Tank();
-    	Tank t2 = new Tank();
-    	t1.level = 9;
-    	t2.level = 47;
-    	Printer.print("Привет");
-	}
-}
 
-class Tank{
-	int level;
+	}
 }
