0eee0056fb974bc898923a0b34b40374c8b5a041
Olegionek <olegionek@gmail.com>
2021-06-16 19:14:10 +0300

    New live

--- src/com/HelloData/Main.java
+++ src/com/HelloData/Main.java
@@ -4,21 +4,22 @@ import java.util.*;
 
 public class Main {
 
-
+	static int result = 0;
+	static void test(int testval, int target){
+		if (testval>target)
+			result = +1;
+		else if(testval<target)
+			result = -1;
+		else
+			result = 0;
+	}
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		float expFloat = 1.39e-43f;
-		expFloat = 1.39E-43f;
-		System.out.println(expFloat);
-		double expDouble = 47e47d;
-		double expDouble2 = 47e47;
-		long n2 = 200;
-		System.out.println("Введите число или слово: ");
-		String input = scanner.nextLine();
-		String c = input;
-		System.out.println(expDouble);
-		System.out.println(c);
-		System.out.println(n2*expDouble);
+		test(10,5);
+		System.out.println(result);
+		test(5,10);
+		System.out.println(result);
+		test(5,5);
+		System.out.println(result);
 	}
 }
 
