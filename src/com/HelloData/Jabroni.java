9e8bd21231b89783dea1f0a66940d1d89789e3e2
Olegionek <olegionek@gmail.com>
2021-06-16 19:18:54 +0300

    New live

---
+++ src/com/HelloData/Jabroni.java
@@ -0,0 +1,24 @@
+package com.HelloData;
+import java.util.*;
+
+public class Jabroni {
+    public static void main(String[] args) {
+        Random  rand = new Random();
+        for (int i=0;i<=30;i++)
+        {
+            int x=rand.nextInt(2)+1;
+            if (x<=1){
+                System.out.println(x);
+                System.out.println("Орел!");
+            }
+            if (x>=1&&x<=2){
+                System.out.println(x);
+                System.out.println("Решка!");
+            }
+            else
+            {
+                System.out.println("Ошибка! Ошибочное значение: " + x);
+            }
+        }
+    }
+}
