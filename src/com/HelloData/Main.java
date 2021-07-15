9c3d6e0148ec034eea3b8deb099261fd7400613c
Olegionek <olegionek@gmail.com>
2021-07-15 18:28:03 +0300

    break

--- src/com/HelloData/Main.java
+++ src/com/HelloData/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		System.out.println();
 	}
 	 static public void fibbonachi(int count) {
-		 int n1 = 0, n2 = 1, n3, i;
+		 int n1 = 0, n2 = 1, n3, i; // Инициализация первых двух цифр последовательности
 		 System.out.print(n1 + " " + n2);
 		 for (i = 2; i<count; ++i) {
 			 n3 = n1 + n2;
