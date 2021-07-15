e908f39c5c67b947fbfc969d98d9c6d9ea44b45e
Olegionek <olegionek@gmail.com>
2021-07-15 19:56:00 +0300

    I learned new objectives: overloading and initializations;

--- src/com/HelloData/AutoPackager.java
+++ src/com/HelloData/AutoPackager.java
@@ -1,14 +1,33 @@
 package com.HelloData;
+import java.util.*;
 
 
-//Exercise N9 Thinking on Java p.93
-public class AutoPackager {
-    Character ch = new Character('f');
+class Tree {
+    int height;
+    Tree() {
+        System.out.print("Сажаем росток: ");
+        height = 0;
+    }
+    Tree(int initialHeight) {
+        height = initialHeight;
+        System.out.println("Создание нового дерева высотой: " + height);
+    }
+    void info() {
+        System.out.println("Дерево высотой " + height + " м.");
+    }
+    void info(String s) {
+        System.out.println(s + ": Дерево высотой " + height + " м.");
+    }
 }
 
-class Printer{
-    static void print(String s){
-        System.out.println(s);
-        return;
+public class AutoPackager {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            Tree t = new Tree(i);
+            t.info();
+            t.info("Перегруженный метод: ");
+        }
+
+
     }
 }
