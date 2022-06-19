7a86855b1f16b390d0d332eb52d1b0647fbad0a8
user <olegionek@gmail.com>
2022-06-19 18:00:04 +0300

    Сделал класс-загрузчик с точкой инициализации. Через неё будут запускаться все учебные классы.

---
+++ src/com/HelloData/Runner.java
@@ -0,0 +1,11 @@
+package com.HelloData;
+
+
+import java.io.IOException;
+
+public class Runner {
+    public static void main (String[] args) {
+        SprinklerSystem sprinklers = new SprinklerSystem();
+        System.out.println(sprinklers);
+    }
+}
