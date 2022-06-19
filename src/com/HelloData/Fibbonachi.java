49f25104b3dc7ada3fcb51c6f0c7094b7ae2c486
user <olegionek@gmail.com>
2022-06-19 18:03:05 +0300

    Обновил класс с числами Фиббоначи. Так-же прокомментировал его.

--- src/com/HelloData/Fibbonachi.java
+++ src/com/HelloData/Fibbonachi.java
@@ -6,25 +6,26 @@ import java.util.*;
 public class Fibbonachi {
 
 
-	public static void main(String[] args) throws UnsupportedEncodingException, IOException {
+	static public void fibbonachi() throws IOException {
 		int count;
 		Scanner scn = new Scanner(System.in);
-		System.out.print(" Enter count of Fibbonachi numbers what you want: ");
+		System.out.print("Введите количество чисел Фиббоначи: ");
 		count = scn.nextInt();
-		fibbonachi(count);
+		Fibbonachi.fibbonachi2(count);
 		System.out.println();
-
-
 	}
-	 static public void fibbonachi(int count) {
+	private Fibbonachi() {} // Эксперименты с доступом. Нижняя функция private
+	//Т.к. Эта функция не используется вне этого класса, следует сделать ей private.
+		static private Fibbonachi fibbonachi2(int count) {
 		 int n1 = 0, n2 = 1, n3, i; // Инициализация первых двух цифр последовательности
 		 System.out.println(n1 + " " + n2);
-		 for (i = 2; i<count; i++) {
+		 for (i = 2; i < count; i++) {
 			 n3 = n1 + n2;
 			 System.out.println(" " + n3);
 			 n1 = n2;
 			 n2 = n3;
 		 }
+		 return new Fibbonachi();
 	 }
 }
 
