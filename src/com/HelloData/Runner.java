b32ac6eabcd0dd8ae4e43f172deed9f2f54465dc
user <olegionek@gmail.com>
2022-06-20 06:46:25 +0300

    Сейчас ничего не изменилось.

--- src/com/HelloData/Runner.java
+++ src/com/HelloData/Runner.java
@@ -4,8 +4,8 @@ package com.HelloData;
 import java.io.IOException;
 
 public class Runner {
+
     public static void main (String[] args) {
-        SprinklerSystem sprinklers = new SprinklerSystem();
-        System.out.println(sprinklers);
+
     }
 }
