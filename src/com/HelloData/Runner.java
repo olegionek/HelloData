e2793ba35f29d6c1247cf4bd868caf74dd8a7946
user <olegionek@gmail.com>
2022-06-20 06:54:34 +0300

    Практика с наследованием. Наверное. extends и прочее.

--- src/com/HelloData/Runner.java
+++ src/com/HelloData/Runner.java
@@ -3,9 +3,23 @@ package com.HelloData;
 
 import java.io.IOException;
 
-public class Runner {
+public class Runner extends Cleanser{
+
+    public void scrub() {
+        append("Runner.scrub()");
+        super.scrub(); // Вызываем метод базоовго класса.
+    }
+    //Добавляем новые методы к классу.
+    public void foam() {append("foam()");}
 
     public static void main (String[] args) {
-
+        Runner x = new Runner();
+        x.dilute();
+        x.foam();
+        x.scrub();
+        x.apply();
+        System.out.println(x);
+        System.out.println("Проверяем базовый класс");
+        Cleanser.main(args);
     }
 }
