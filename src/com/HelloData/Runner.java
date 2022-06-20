49a07c2214cd4a0763bcee7c9e779f2188b98bee
user <olegionek@gmail.com>
2022-06-20 08:48:53 +0300

    Вернул к прежнему состоянию.

--- src/com/HelloData/Runner.java
+++ src/com/HelloData/Runner.java
@@ -3,23 +3,9 @@ package com.HelloData;
 
 import java.io.IOException;
 
-public class Runner extends Cleanser{
-
-    public void scrub() {
-        append("Runner.scrub()");
-        super.scrub(); // Вызываем метод базоовго класса.
-    }
-    //Добавляем новые методы к классу.
-    public void foam() {append("foam()");}
+public class Runner{
 
     public static void main (String[] args) {
-        Runner x = new Runner();
-        x.dilute();
-        x.foam();
-        x.scrub();
-        x.apply();
-        System.out.println(x);
-        System.out.println("Проверяем базовый класс");
-        Cleanser.main(args);
+
     }
 }
