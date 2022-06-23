6740d8b443a3ba7075f719c7316b0d49eca803aa
user <olegionek@gmail.com>
2022-06-23 06:48:54 +0300

    Изолировал класс и привел в соответсвие с учебником

--- src/com/HelloData/Cleanser.java
+++ src/com/HelloData/Cleanser.java
@@ -1,15 +1,58 @@
 package com.HelloData;
 
 public class Cleanser {
-    private String s = "Cleanser";
+    private String s = "Cleanser\n";
     public void append(String a) { s+=a;}
-    public void dilute() {append("dilute()");}
-    public void apply() {append("apply()");}
-    public void scrub() {append("scrub()");}
+    public void dilute() {append("dilute()\n");}
+    public void apply() {append("apply()\n");}
+    public void scrub() {append("scrub()\n");}
     public String toString() {return s;}
-    public static void main (String[] args) {
+    public static void nemain (String[] args) {
         Cleanser x = new Cleanser();
         x.dilute(); x.apply();x.scrub();
         System.out.println(x);
     }
+}
+
+class Detergent extends Cleanser {
+    public void scrub() {
+        append("Runner()\n");
+        super.scrub();  //Вызываем метод базового класса.
+    }
+    //Добавляем новые методы к интерфейсу
+    public void foam() {
+        append("\nfoam()");
+    }
+    public static void nemain(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        Cleanser.nemain(args);
+    }
+}
+
+class NewClass extends Detergent {
+    public void scrub (){
+        super.scrub();
+        append("new Scrub() from NewClass\n");
+    }
+
+    public void sterilize(){append("Sterilize()\n");}
+    public static void nemain(String[] args){
+        NewClass c = new NewClass();
+        c.foam();
+        c.apply();
+        c.scrub();
+        c.dilute();
+        c.sterilize();
+        System.out.println(c);
+        System.out.println("Проверяем базовый класс\n");
+        Detergent.nemain(args);
+
+    }
+
+
 }
